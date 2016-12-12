66e6087637d0432a4897563ca93efec4c0aebf82
Morris Allison III <author@morris.xyz>
2016-12-11 21:32:51 -0800

    Directly fix export issue
    After fiddling around with Rollup, TypeScript, and build configs, I decided to just fixed the issue directly. 💥 it's fixed.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import {EventStation} from './EventStation';
 
-export {EventStation};
+(<any>EventStation).EventStation = EventStation;
+(<any>EventStation).default = EventStation;
+
+Object.defineProperty(EventStation, '__esModule', { value: true });
+
 export default EventStation;
