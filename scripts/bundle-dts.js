67749d4bac0876bdf5f905a7ec0ca7a8fe334e5b
Morris Allison III <author@morris.xyz>
2016-12-31 17:09:44 -0800

    refactor: DRY up bundle-dts script

--- scripts/bundle-dts.js
+++ scripts/bundle-dts.js
@@ -5,8 +5,9 @@ var mkdirp = require('mkdirp');
 var path = require('path');
 
 var banner = require('../config/banner');
+var pkg = require('../package.json');
 
-var dest = path.resolve(__dirname, '../dist/event-station.d.ts');
+var dest = path.resolve(__dirname, `../dist/${pkg.name}.d.ts`);
 var dist = path.resolve(__dirname, '../dist');
 var src = path.resolve(__dirname, '../build/dts/main.d.ts');
 
@@ -14,7 +15,7 @@ mkdirp(dist, function () {
     fs.readFile(src, function (err, buf) {
         if (err) throw err;
 
-        var declaration = '\ndeclare module "event-station" {';
+        var declaration = `\ndeclare module "${pkg.name}" {`;
         var definition = buf.toString()
             .replace(/(}\n)?declare module "([^"]*)" {/g, '')
             .replace(/\n\s+import ([^"]+) from "([^"]+)";/g, '');
