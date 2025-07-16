9ecdb12492a12ea358204c9f8420e6bc8d6528d0
Morris Allison III <morrisallison@users.noreply.github.com>
2025-07-16 15:31:29 -0700

    fix: export missing types

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,3 @@
-import { EventStation } from "./models/EventStation";
-
-// Keep the import and export statements separate to support
-// tooling that doesn't recognize export forwarding statements
-// like `export { EventStation } from "./models/EventStation";`
-export { EventStation };
+export { EventStation } from "./models/EventStation";
+export type { Listeners } from "./models/Listeners";
+export type { Options } from "./types/Options";
